0f557977e3c3cdbf90e973d9f8c6b11b3e5ea651
Muhammad Shaheer <126573874+Shaheerkz@users.noreply.github.com>
2024-02-29 22:49:43 +0500

    project deployed

---
+++ src/sections/Domain.jsx
@@ -0,0 +1,224 @@
+import React from "react";
+import domainIcon1 from "../assets/images/domain-icon-1.png";
+import One from "../assets/images/1.png";
+import DomainLogo from "../assets/images/domain-logo.png";
+import UserIcon from "../assets/images/user-icon.png";
+function Domain() {
+  return (
+    <>
+      <section className="domain-sec">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="domain-content-wrp position-relative" data-aos="fade-left">
+                <div className="wrp">
+                  <h4>
+                    <span className="b-span-one">Our</span> 8 Domains
+                  </h4>
+                  <small className="text-white">
+                    At Ace8, we empower businesses to thrive in the digital age
+                    through cutting-edge technology services spanning eight
+                    crucial domains. From Advanced Architectures to <br />{" "}
+                    Microservices, Code & Development, we're dedicated to
+                    delivering unparalleled expertise and innovative solutions
+                    tailored to meet your specific needs.
+                  </small>
+                </div>
+                <div className="row wrp align-items-center justify-content-center mt-5">
+                  <div className="col-md-3">
+                    <div className="domain-text">
+                      <div className="domain-icons d-flex">
+                        <img src={domainIcon1} alt="" />
+                        <img src={One} alt="" />
+                      </div>
+                      <div className="domain-icon-text mt-3 py-2">
+                        <p>
+                          Advanced Architectures <br />{" "}
+                          <span className="b-span-one">(AceEA)</span>
+                        </p>
+                        <small className="text-white">
+                          Our team of experts excels in designing and
+                          implementing advanced technology enterprise
+                          architectures, ensuring your systems are not just up
+                          to date, but ahead of the curve.
+                        </small>
+                      </div>
+                    </div>
+                    <div className="domain-text mt-5">
+                      <div className="domain-icons d-flex">
+                        <img src={domainIcon1} alt="" />
+                        <img src={One} alt="" />
+                      </div>
+                      <div className="domain-icon-text mt-3 py-2">
+                        <p>
+                          Advanced Architectures <br />{" "}
+                          <span className="b-span-one">(AceEA)</span>
+                        </p>
+                        <small className="text-white">
+                          Our team of experts excels in designing and
+                          implementing advanced technology enterprise
+                          architectures, ensuring your systems are not just up
+                          to date, but ahead of the curve.
+                        </small>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="main-center d-flex justify-content-between">
+                      <div className="domain-text">
+                        <div className="domain-icons d-flex">
+                          <img src={domainIcon1} alt="" />
+                          <img src={One} alt="" />
+                        </div>
+                        <div className="domain-icon-text mt-3 py-2">
+                          <p>
+                            Advanced Architectures <br />{" "}
+                            <span className="b-span-one">(AceEA)</span>
+                          </p>
+                          <small className="text-white">
+                            Our team of experts excels in designing and
+                            implementing advanced technology enterprise
+                            architectures, ensuring your systems are not just up
+                            to date, but ahead of the curve.
+                          </small>
+                        </div>
+                      </div>
+                      <div className="domain-text">
+                        <div className="domain-icons d-flex">
+                          <img src={domainIcon1} alt="" />
+                          <img src={One} alt="" />
+                        </div>
+                        <div className="domain-icon-text mt-3 py-2">
+                          <p>
+                            Advanced Architectures <br />{" "}
+                            <span className="b-span-one">(AceEA)</span>
+                          </p>
+                          <small className="text-white">
+                            Our team of experts excels in designing and
+                            implementing advanced technology enterprise
+                            architectures, ensuring your systems are not just up
+                            to date, but ahead of the curve.
+                          </small>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="logo-domain text-center p-5">
+                      <img src={DomainLogo} alt="" />
+                    </div>
+                    <div className="main-center d-flex justify-content-between">
+                      <div className="domain-text">
+                        <div className="domain-icons d-flex">
+                          <img src={domainIcon1} alt="" />
+                          <img src={One} alt="" />
+                        </div>
+                        <div className="domain-icon-text mt-3 py-2">
+                          <p>
+                            Advanced Architectures <br />{" "}
+                            <span className="b-span-one">(AceEA)</span>
+                          </p>
+                          <small className="text-white">
+                            Our team of experts excels in designing and
+                            implementing advanced technology enterprise
+                            architectures, ensuring your systems are not just up
+                            to date, but ahead of the curve.
+                          </small>
+                        </div>
+                      </div>
+                      <div className="domain-text">
+                        <div className="domain-icons d-flex">
+                          <img src={domainIcon1} alt="" />
+                          <img src={One} alt="" />
+                        </div>
+                        <div className="domain-icon-text mt-3 py-2">
+                          <p>
+                            Advanced Architectures <br />{" "}
+                            <span className="b-span-one">(AceEA)</span>
+                          </p>
+                          <small className="text-white">
+                            Our team of experts excels in designing and
+                            implementing advanced technology enterprise
+                            architectures, ensuring your systems are not just up
+                            to date, but ahead of the curve.
+                          </small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-3">
+                    <div className="domain-text">
+                      <div className="domain-icons d-flex">
+                        <img src={domainIcon1} alt="" />
+                        <img src={One} alt="" />
+                      </div>
+                      <div className="domain-icon-text mt-3 py-2">
+                        <p>
+                          Advanced Architectures <br />{" "}
+                          <span className="b-span-one">(AceEA)</span>
+                        </p>
+                        <small className="text-white">
+                          Our team of experts excels in designing and
+                          implementing advanced technology enterprise
+                          architectures, ensuring your systems are not just up
+                          to date, but ahead of the curve.
+                        </small>
+                      </div>
+                    </div>
+                    <div className="domain-text mt-5">
+                      <div className="domain-icons d-flex">
+                        <img src={domainIcon1} alt="" />
+                        <img src={One} alt="" />
+                      </div>
+                      <div className="domain-icon-text mt-3 py-2">
+                        <p>
+                          Advanced Architectures <br />{" "}
+                          <span className="b-span-one">(AceEA)</span>
+                        </p>
+                        <small className="text-white">
+                          Our team of experts excels in designing and
+                          implementing advanced technology enterprise
+                          architectures, ensuring your systems are not just up
+                          to date, but ahead of the curve.
+                        </small>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-5 my-5">
+                    <button className="domain-btn">
+                      <img src={UserIcon} alt="" />
+                      <p>
+                        Top-Tier Talent (Conntal) <br />{" "}
+                        <span>
+                          Conntal supports all of our divisions of Ace8 and
+                          provides our eternal teams and clients with best of
+                          breed talent.
+                        </span>
+                      </p>{" "}
+                    </button>
+                  </div>
+                </div>
+                <div className="row wrp mt-5">
+                  <div className="col-12">
+                    <div className="domain-bottom">
+                      <p>
+                        Ace8 is not just a technology services company; we are
+                        your strategic partner in navigating the ever-evolving
+                        landscape of technology. Elevate your business to new
+                        heights with Ace8 - Where innovation meets excellence!
+                        Ace8 is not just a technology services company; we are
+                        your strategic partner in navigating the ever-evolving
+                        landscape of technology. Elevate your business to new
+                        heights with Ace8 - Where innovation meets excellence!
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Domain;
